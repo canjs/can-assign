09063fd7ef3012c57afe7648460cbe7ba4978996
Kevin Phillips <kevin@bitovi.com>
2017-11-30 10:48:06 -0600

    1.1.0

---
+++ dist/cjs/can-assign.js
@@ -0,0 +1,12 @@
+/*can-assign@1.0.1#can-assign*/
+module.exports = function (d) {
+    var sources = Array.prototype.slice.call(arguments, 1);
+    var s = sources.shift();
+    while (s) {
+        for (var prop in s) {
+            d[prop] = s[prop];
+        }
+        s = sources.shift();
+    }
+    return d;
+};
